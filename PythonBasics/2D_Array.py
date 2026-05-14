6afc010e0a4076c7722ca6416975766fc8c33e71
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-14 19:54:11 +0530

    Implement 2D array creation and multiplication
    Added code to create and multiply 2D arrays using NumPy.

--- PythonBasics/2D_Array.py
+++ PythonBasics/2D_Array.py
@@ -1,1 +1,11 @@
+#create 2D array
+import numpy as np
+arr= np.array([[1,2,3],[4,5,6]])
+print("Matrix(2D)")
+print(arr)
 
+#Multiplication of two 2x2 Matrix
+x=np.array([[2,4],[3,5]])
+y=np.array([[2,4],[4,5]])
+z=x@y # '@'multiplies the matrix by row and column
+print(z)
