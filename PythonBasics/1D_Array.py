2d57056a7f3fd91ac3c4624e12b39bf975f076d4
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-14 19:51:17 +0530

    Add examples for creating 1D arrays with numpy
    Added examples of creating 1D arrays using numpy, including arrays from lists, arrays of ones, and evenly spaced values.

--- PythonBasics/1D_Array.py
+++ PythonBasics/1D_Array.py
@@ -1,1 +1,25 @@
+#create 1D array
+import numpy as np
+arr=np.array([1,2,3,4])
+print("1D Array:",arr)
+type(arr)
+
+#create using list
+list=[45,3,65,7]
+a=np.array(list,dtype=float)
+print(a)
+a=np.array(list,dtype="U32")
+print(a)
+
+#create an array of ones
+ones_arr=np.ones((2,4)) #2*4 array of matrix
+print("Ones Array:\n",ones_arr)
+
+#create an array with 5 values evenly spaced between 0 and 1
+linspace_arr=np.linspace(0,1,5) #5 points between 0 and 1
+print(linspace_arr)
+
+#create an array with 5 values evenly spaced between 0 and 1
+linspace_arr=np.linspace(0,1,3) #3 points between 0 and 1
+print(linspace_arr)
 
