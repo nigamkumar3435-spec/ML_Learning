63dccd45f92f14e34773f7443ab4dc0d38a81704
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-14 19:56:27 +0530

    Add basic 1D array operations using NumPy

--- PythonBasics/1D_ArrayOperations.py
+++ PythonBasics/1D_ArrayOperations.py
@@ -1,1 +1,20 @@
+#create two arrays
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
 
+#element wise additi0n
+sum_arr=arr1+arr2
+print("Sum:",sum_arr)
+
+#element wise difference
+diff_arr=arr1-arr2
+print("Difference:",diff_arr)
+
+#element wise multiplication
+mult_arr=arr1*arr2
+print("Multiplication:",mult_arr)
+
+#element wise division
+div_arr=arr1/arr2
+print("Division:",div_arr)
