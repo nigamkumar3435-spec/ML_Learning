36b3858a3dbe47ce640348a74d3bf90ab0c8ffa8
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-15 23:05:44 +0530

    Compare size of list and NumPy array
    Added comparison of sizes between a list and a NumPy array.

--- PythonBasics/BasicOperations.py
+++ PythonBasics/BasicOperations.py
@@ -10,3 +10,11 @@ print(arr.itemsize)
 print(arr.size)
 print(arr)
 print(type(arr))
+
+#comparing size of array and list
+import sys
+import numpy as np
+l=range(1000)
+print("Size of list: ",sys.getsizeof(l)*len(l))
+array=np.arange(1000)
+print("Size of Numpy array: ",array.size*array.itemsize)
