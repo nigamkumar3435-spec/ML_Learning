d40141160b9be57b26c0d06a85114094e48f9a27
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-15 23:04:59 +0530

    Enhance BasicOperations with numpy and sys usage
    Added numpy and sys imports, and included array size and type information.

--- PythonBasics/BasicOperations.py
+++ PythonBasics/BasicOperations.py
@@ -1,1 +1,12 @@
+import numpy as np
+import sys
 
+a=range(1000)
+print(sys.getsizeof(a))
+
+arr=np.arange(1000)
+arr=np.arange(10)
+print(arr.itemsize)
+print(arr.size)
+print(arr)
+print(type(arr))
