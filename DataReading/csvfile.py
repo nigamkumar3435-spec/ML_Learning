1c340f6e223fe379eccf96447656e9f6ec851c71
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-20 18:54:28 +0530

    Read CSV data and fill missing values with mean
    Added code to read CSV data and handle missing values by filling them with the mean of their respective columns.

--- DataReading/csvfile.py
+++ DataReading/csvfile.py
@@ -1,1 +1,12 @@
+# To Read CSV data
 
+import pandas as pd
+CSVData=pd.read_csv('Iris_data_sample.csv',na_values=('??','###'))
+# print(CSVData['SepalLengthCm'].dtype)
+# print(CSVData.duplicated().sum())
+# print(CSVData.isna().sum())
+print(CSVData.head(6))
+CSVData['SepalLengthCm']=CSVData['SepalLengthCm'].fillna(CSVData['SepalLengthCm'].mean())
+CSVData['SepalWidthCm']=CSVData['SepalWidthCm'].fillna(CSVData['SepalWidthCm'].mean())
+CSVData['PetalLengthCm']=CSVData['PetalLengthCm'].fillna(CSVData['PetalLengthCm'].mean())
+print(CSVData.head(6))
