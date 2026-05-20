4c4c947de3462ba17d9aaae88950b4ce1c339b29
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-20 18:25:30 +0530

    Create employee dictionary and filter by department
    Added code to create a dictionary of employees with their names, departments, and salaries. Included functionality to print names of employees working in the CSE department.

--- PythonBasics/Dictionary.py
+++ PythonBasics/Dictionary.py
@@ -1,1 +1,31 @@
+#python code to create dictionary
 
+emp={
+    'EmpName' : ['Nigam','Aakash','Anand','Krishna','Priya','Aakash'],
+    'DeptName': ['CSE','IT','IOT','AIML','CSE','Mechanical'],
+    'Salary'  : [51000,12000,8000,35000,45000,23000]
+}
+print(emp)
+print(emp['EmpName'])
+# for i in emp['EmpName']:
+#     # print(i)
+# print(emp.keys())
+# print(emp.values())
+# print(emp.items())
+
+#a.To print name and deptname of employee who get maximum salary.
+
+# maxsal=max(emp['Salary'])
+# print(maxsal)
+# for i in emp['Salary']:
+#   if i==maxsal:
+#     name=emp['EmpName'][emp['Salary'].index(i)]
+#     dept=emp['DeptName'][emp['Salary'].index(i)]
+# print(name,dept)
+
+#b.To print name of employee who work in CSE dept.
+
+for i in emp['DeptName']:
+  if i=='CSE':
+    name=emp['EmpName'][emp['DeptName'].index(i)]
+    print(name)
