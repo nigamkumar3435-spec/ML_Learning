8499977f32a8f7626183c893381bdd4dda8177af
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-20 18:45:02 +0530

    Create numerical arrays and display properties
    Added code to create and print properties of numerical arrays using NumPy.

--- PythonBasics/numerical.py
+++ PythonBasics/numerical.py
@@ -1,1 +1,15 @@
+#python code to create numerical array
 
+import numpy as np
+a=np.array([1,2,3,4])
+print(a)
+print(type(a))
+print(a.dtype)
+print(a.ndim)
+print(a.shape)
+print(a.size)
+b=np.array([[1,2,3,4],[5,6,7,8]])
+print(b)
+print(b.ndim)
+print(b.shape)
+print(b.size)
