335a2ee41372fa182dc821e86f9d1fb124d5df6a
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-21 23:57:20 +0530

    Add dataset creation and loading example

--- PandasSeries/dataset.py
+++ PandasSeries/dataset.py
@@ -1,1 +1,22 @@
+###CREATING AND LOADING DATASET###
 
+# Import pandas
+import pandas as pd
+
+# Create a list of countries (string values)
+country = ['India', 'Pakistan', 'USA', 'Nepal', 'Srilanka']
+
+# Convert list into Pandas Series
+country_ser = pd.Series(country)
+
+# Print country series
+print(country_ser)
+
+# Create a list of runs (integer values)
+runs = [13, 24, 56, 78, 100]
+
+# Convert list into Pandas Series
+runs_ser = pd.Series(runs)
+
+# Print runs series
+print(runs_ser)
