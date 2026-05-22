79a0434d3c2b1f379ad4b06ae36a6daa51cdc381
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-22 23:07:48 +0530

    Displays the salary distribution using pie chart
    Displays the salary distribution according to qualification level using pie chart

--- Graphs/PieChart2.py
+++ Graphs/PieChart2.py
@@ -1,1 +1,23 @@
+#Calculate Total Salary for Each Qualification
 
+import pandas as pd
+import matplotlib.pyplot as plt
+from google.colab import drive
+drive.mount('/content/drive',force_remount=True)
+data=pd.read_csv('/content/drive/MyDrive/ML_Lab/EMP1.csv')
+quali=['Undergraduate','Graduate','Postgraduate']
+
+totalsal=[]
+
+ug=data[(data['Qualification']==0)]['Salary'].sum()
+totalsal.append(ug)
+
+g=data[(data['Qualification']==1)]['Salary'].sum()
+totalsal.append(g)
+
+pg=data[(data['Qualification']==2)]['Salary'].sum()
+totalsal.append(pg)
+
+c=['cyan','skyblue','#9874ff']
+
+plt.pie(totalsal,labels=quali,colors=c) #Displays the salary distribution according to qualification level using pie chart.
