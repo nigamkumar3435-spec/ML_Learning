bd45633015675567573ad34efa4ba2812aab51bf
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-23 23:58:42 +0530

    Add basic NumPy array creation and properties

--- NumpyBasics/array2.py
+++ NumpyBasics/array2.py
@@ -1,1 +1,24 @@
+a1=np.arange(10)
+a2=np.arange(12,dtype=np.float64).reshape(3,4)
+a3=np.arange(8).reshape(2,2,2)
+print(a1)
+print()
+print(a2)
+print()
+print(a3)
+
+#ndim --what type of array it is
+print(a1.ndim)
+print(a2.ndim)
+print(a3.ndim)
+
+#shape --what size of array is (in tuple)
+print(a1.shape)
+print(a2.shape)
+print(a3.shape)
+
+#size --how much elements are there in array
+print(a1.size)
+print(a2.size)
+print(a3.size)
 
