696c65efc98e933df79b11f774b583507c4e0236
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-26 23:09:34 +0530

    Implement basic NumPy array operations
    Added basic array operations using NumPy, including arithmetic, max, min, sum, and product calculations.

--- NumpyBasics/arrayoperations.py
+++ NumpyBasics/arrayoperations.py
@@ -1,1 +1,34 @@
+import numpy as np
 
+#vector operations (Tensor) space of both the matrix
+a=np.array([[1,2,3],[4,5,6]])
+b=np.array([[2,2,2],[2,2,2]])
+print(a)
+print()
+print(b)
+
+#arithmetic operations
+print(a+b)
+print()
+print(a-b)
+print()
+print(a*b)
+print()
+print(a/b)
+print()
+print(a**b)
+print()
+print(a//b)
+
+#max/min/sum/prod
+#0-> col and 1-> row
+b=np.arange(12).reshape(3,4)
+print(b)
+print(b.sum())
+print(b.sum(axis=0))#column wise
+print(b.sum(axis=1))#row wise
+print(b.max())
+print(b.min())
+print(b.max(axis=0))
+print(b.min(axis=0))
+print(b.prod())
