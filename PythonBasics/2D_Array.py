2fe5c9406200ec25f508ab1036b996c859e8b0f6
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-27 22:20:49 +0530

    Add new 2D array and print its contents
    Added a new 2D array and printed its output.

--- PythonBasics/2D_Array.py
+++ PythonBasics/2D_Array.py
@@ -4,6 +4,12 @@ arr= np.array([[1,2,3],[4,5,6]])
 print("Matrix(2D)")
 print(arr)
 
+two_d = np.array([[2, 4], [6, 8]])
+print("2D:\n", two_d)
+#Output: 2D:
+ #          [[2 4]
+ #           [6 8]]
+
 #Multiplication of two 2x2 Matrix
 x=np.array([[2,4],[3,5]])
 y=np.array([[2,4],[4,5]])
