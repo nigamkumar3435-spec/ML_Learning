1ca382814153fbbe5729d072651ee6de2954f378
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-29 22:50:03 +0530

    Implement KMeans clustering for Age vs Price analysis
    Added KMeans clustering to analyze Age vs Price data, including data preprocessing and visualization.

--- Unsupervised_MachineLearning/Clustering2.py
+++ Unsupervised_MachineLearning/Clustering2.py
@@ -1,1 +1,35 @@
+# Age vs Price
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from google.colab import drive
 
+drive.mount('/content/drive', force_remount=True)
+
+data=pd.read_csv('/content/drive/MyDrive/ML_Lab/Toyota (1).csv',index_col=0,na_values=["??","????","????"])
+
+#data = pd.read_csv('./Toyota.csv')
+
+data['KM'].fillna(data['KM'].mean(),inplace=True)
+data['Age'].fillna(data['Age'].mean(),inplace=True)
+
+km=KMeans(n_clusters=2)
+y_predicted=km.fit_predict(data[['Price','Age']])
+y_predicted
+
+data['cluster']=y_predicted
+data
+
+d1=data[data.cluster==0]
+d2=data[data.cluster==1]
+d3=data[data.cluster==2]
+
+plt.scatter(d1.Price,d1['Age'],color='green')
+plt.scatter(d2.Price,d2['Age'],color='red')
+plt.scatter(d3.Price,d3['Age'],color='blue')
+
+plt.xlabel('Price')
+plt.ylabel('Age')
+plt.legend()
