02094911ac3fe23025171132308dec29f0ca98f2
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-06-02 23:56:55 +0530

    Implement basic CSV operations in operations.py
    Added basic operations for reading and analyzing a CSV file.

--- DataReading/operations.py
+++ DataReading/operations.py
@@ -1,1 +1,19 @@
+#Some Basic Operations on csv file
 
+df_iris=pd.read_csv("/content/Iris.csv")
+print(df_csv)
+
+#Mean
+print(df_iris.mean(numeric_only=True))
+
+#Median
+print(df_iris.median(numeric_only=True))
+
+#Mode
+print(df_iris.mode(numeric_only=True))
+
+#count of non-null values
+print(df_iris.count())
+
+#Species
+print(df_iris['Species'].value_counts())
