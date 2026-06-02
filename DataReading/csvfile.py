b51b4023eba99f3600c1c6374d808984e917fcae
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-06-02 23:49:30 +0530

    Enhance csvfile.py to read text and CSV data
    Added functionality to read text and CSV data from additional sources.

--- DataReading/csvfile.py
+++ DataReading/csvfile.py
@@ -1,4 +1,4 @@
-# To Read CSV data
+# 1. To Read CSV data
 
 import pandas as pd
 CSVData=pd.read_csv('Iris_data_sample.csv',na_values=('??','###'))
@@ -10,3 +10,10 @@ CSVData['SepalLengthCm']=CSVData['SepalLengthCm'].fillna(CSVData['SepalLengthCm'
 CSVData['SepalWidthCm']=CSVData['SepalWidthCm'].fillna(CSVData['SepalWidthCm'].mean())
 CSVData['PetalLengthCm']=CSVData['PetalLengthCm'].fillna(CSVData['PetalLengthCm'].mean())
 print(CSVData.head(6))
+
+#1. To read the Text data and CSV data
+df_text=pd.read_csv("countries of the world.txt",sep='/t')
+print(df_text)
+
+df_csv=pd.read_csv("https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv")
+df_csv
