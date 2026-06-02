d17df3a30f88ded36538e910927ebf25c37964b1
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-06-02 23:51:29 +0530

    Enhance Excel file reading and column deletion
    Added functionality to read another Excel file and delete a column.

--- DataReading/excelfile.py
+++ DataReading/excelfile.py
@@ -6,3 +6,11 @@ ExcelData
 #...
 ExcelData=pd.read_excel('Iris_data_sample.xlsx',sheet_name='Sheet1')
 ExcelData
+
+#To Read another Excel file
+df_excel=pd.read_excel("Book1.xlsx")
+df_excel
+
+#delete a column
+df_excel=df_excel.drop(columns=['Department'])
+df_excel.head()
